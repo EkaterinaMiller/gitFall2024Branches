19e7d5f09fa7b998a70141a0bb698614933a0fd0
EkaterinaMiller <evmiller@nic.edu>
2024-10-22 13:37:50 -0700

    create add function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
+int add(int a, int b);
 int main()
 {
     std::cout<<"hello world\n";
+    std::cout<<"The sum of 5 and 7 is "<<add(5,7)<<"\n";
+}
+int add(int a, int b)
+{
+    return a+b;
 }
